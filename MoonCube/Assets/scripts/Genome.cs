bae47f9d44d5a1765ecae9c6de738953b82bf42f
Architechno <elektron@hotmail.co.uk>
2020-03-02 20:26:39 +0000

    Update Genome.cs

--- MoonCube/Assets/scripts/Genome.cs
+++ MoonCube/Assets/scripts/Genome.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Genome : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<int> bits;
+public double fitness;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+     
     void Update()
     {
-        
+        fitness = 0;
+	bits = new List<int> ();
     }
 }
